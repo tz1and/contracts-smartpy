44f39f698db684cd3b84b37cec66536bb446b7bb
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-12-08 05:18:10 +0100

    AdminLambda: use tstorage on type

--- tezosbuilders_contracts_smartpy/mixins/AdminLambda.py
+++ tezosbuilders_contracts_smartpy/mixins/AdminLambda.py
@@ -1,15 +1,17 @@
 import smartpy as sp
 
 
-t_admin_lambda = sp.TLambda(sp.TUnit, sp.TList(sp.TOperation), with_storage="read-write", with_operations=True)
-
-
 # Required mixins: Administrable
 class AdminLambda:
+    """NOTE: initialised AdminLambda after any mixins that update the storage."""
+    def __init__(self):
+        self.t_admin_lambda = sp.TLambda(sp.TUnit, sp.TList(sp.TOperation),
+            with_storage="read-write", with_operations=True, tstorage=self.storage_type)
+
     @sp.entry_point
     def run_lambda(self, lambda_function):
         """Allows the administrator to run a lambda."""
-        sp.set_type(lambda_function, t_admin_lambda)
+        sp.set_type(lambda_function, self.t_admin_lambda)
         self.onlyAdministrator()
         operations = lambda_function(sp.unit)
         sp.add_operations(operations)
